51f393b6362a9c7cd347b82dbf4ac672ea197cee
Samed Dallaa <112594764+sameddallaa@users.noreply.github.com>
2025-12-25 12:32:58 +0100

    feat: model loaders

---
+++ prompt2edit/load_models.py
@@ -0,0 +1,27 @@
+import torch
+from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection, SamModel, SamProcessor
+from PIL import Image
+from diffusers import StableDiffusionInpaintPipeline
+import numpy as np
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def load_object_detection_model():
+    model_id = "IDEA-Research/grounding-dino-tiny" 
+    processor = AutoProcessor.from_pretrained(model_id)
+    model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+    
+    return model, processor
+
+def load_segmentation_model():
+    model = SamModel.from_pretrained("facebook/sam-vit-base").to(device)
+    processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
+    
+    return model, processor
+
+def load_inpainting_model():
+    model = StableDiffusionInpaintPipeline.from_pretrained(
+        "runwayml/stable-diffusion-inpainting",
+    ).to(device)
+    
+    return model
